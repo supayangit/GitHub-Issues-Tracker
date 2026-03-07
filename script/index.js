27d15730c09c978dff9603ca05d8ac684e44dd56
supayangit <supayanchk@gmail.com>
2026-03-07 13:47:00 +0600

    login functionality done

--- script/index.js
+++ script/index.js
@@ -1,3 +1,16 @@
 const doSignIn = () => {
+    const userNameField = document.getElementById("username-input");
+    const passwordField = document.getElementById("password-input");
+ const userName = userNameField.value;
+ const password = passwordField.value;
 
-}
+ if(userName == "admin" && password == "admin123"){
+    alert("Sign In Successfull!");
+    window.location.href = "/dashboard.html";
+ } else {
+    alert("Enter Correct Credentials!");
+    userNameField.value= "";
+    passwordField.value= "";
+    return;
+ }
+};
